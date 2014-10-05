3a29990b36c6f32b633389dbbfde3c30db23524c
U-Jengerer-PC\Jengerer <Jengerer@Jengerer-PC.(none)>
2014-10-05 00:00:48 -0400

    Added camera object.

---
+++ camera.cpp
@@ -0,0 +1,36 @@
+#include "camera.h"
+
+Camera::Camera()
+{
+}
+
+// Update camera world position.
+void Camera::SetPosition(const Vector3 *position)
+{
+	this->position.Copy(position);
+}
+
+
+// Update camera angles.
+void Camera::SetAngles(const Vector3 *angles)
+{
+	this->angles.Copy(angles);
+}
+
+// Generate world to camera view transform.
+void Camera::GenerateViewTransform(Matrix4x4 *out)
+{
+	Vector3 translateOffset;
+	Matrix4x4 rotation, translation;
+
+	// Get the inverse (transpose) rotation matrix.
+	rotation.RotationEuler(&angles);
+	rotation.Transpose(&rotation);
+	
+	// Get the inverse translation matrix.
+	translateOffset.Negation(&position);
+	translation.Translation(&translateOffset);
+
+	// Multiply the two.
+	out->Product(&rotation, &translation);
+}
--- camera.h
+++ camera.h
@@ -1,20 +1,25 @@
-#ifndef _CAMERA_H_
-#define _CAMERA_H_
+#pragma once
 
 #include "matrix4x4.h"
 
 // Structure for representing a point of view.
-typedef struct camera
+class Camera
 {
-	vector3d_t origin;
-	vector3d_t angles;
-} camera_t;
 
-// Camera position and angle functions.
-void camera_set_origin(camera_t *camera, const vector3d_t *origin);
-void camera_set_angles(camera_t *camera, const vector3d_t *angles);
+public:
 
-// Get the matrix transform for camera view.
-void camera_world_to_view_transform(camera_t *camera, matrix4x4_t *out);
+	Camera();
 
-#endif // _CAMERA_H_
+	// Set position and angles.
+	void SetPosition(const Vector3 *position);
+	void SetAngles(const Vector3 *angles);
+
+	// Generate camera transform.
+	void GenerateViewTransform(Matrix4x4 *out);
+
+private:
+
+	Vector3 position;
+	Vector3 angles;
+
+};
